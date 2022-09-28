6244e6dcac75995f462275efaaa13ebdafc2dd70
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-27 20:33:10 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,10 +2,16 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-//INDEX
+// INDEX
 breads.get('/', (req, res) => {
-    res.send('Bread')
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
---
+++ views/index.jsx
@@ -0,0 +1,30 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads})  {
+    return (
+        <Default>
+        <h2>Index Page</h2>
+        {/* This is a JSX comment. */}
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        <ul>
+            {
+              breads.map((bread, index) => {
+                return(
+                    <li key={index}>
+                        {/* index refers to the index position of the array item */}
+                        <a href={`/breads/${index}`}>
+                            {bread.name}
+                        </a>
+                    </li>
+                )
+              }
+              )  
+            }
+        </ul>
+      </Default>      
+    )
+}
+
+
+module.exports = Index
