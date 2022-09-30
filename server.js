5188b7ce06f54de3818d1eeb1a4b4475c9e3804e
Tsilos Kosbab <38735446+Tsilos1@users.noreply.github.com>
2022-09-29 19:58:22 -0500

    updated links

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use (express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 
 //Routes
@@ -32,4 +33,6 @@ app.listen (PORT, () => {
 app.get('*', (req, res) => {
     res.send('404')
   })
+
+
   
